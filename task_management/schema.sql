f4fe7e208d06cc15848b921ae6f3f73b7b221f2d
Leisa <1015375776@qq.com>
2022-01-11 20:21:56 +0800

    Delete task_management directory

--- task_management/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS task;
-
-CREATE TABLE task (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user TEXT UNIQUE NOT NULL ,
-  department TEXT NOT NULL ,
-  task_unfinished INTEGER DEFAULT 1
-);
